128a0ebfb09072876d3435d0d0b6cfb09f51625a
josezanata <jose.krais@escola.pr.gov.br>
2026-06-24 09:54:35 -0300

    Add files via upload

---
+++ Aula01var.java
@@ -0,0 +1,18 @@
+package AULAJAVA;
+
+public class Aula01var{
+
+    public static void main(string[] args){
+    int idade = 17;
+    double altura = 1.75;
+    boolean matriculado = true;
+    string nome = "nuno mendes";
+
+    system.out.println("nome: " + nome);
+    system.out.println("idade: " + idade + "anos");
+    system.out.println("altura: " + altura + "m");
+    system.out.println("aluno matriculado?: " + matriculado);
+    
+}
+
+}
