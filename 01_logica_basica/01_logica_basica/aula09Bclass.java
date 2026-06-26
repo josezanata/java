e4569c7fdf9827ea31adf2f246816c5acab201dd
josezanata <jose.krais@escola.pr.gov.br>
2026-06-26 08:22:58 -0300

    Add files via upload

---
+++ 01_logica_basica/01_logica_basica/aula09Bclass.java
@@ -0,0 +1,26 @@
+
+import java.util.scanner;
+
+public class aula09Bclass{
+
+    public static void main(string[] args){
+        scanner teclado = new scanner(system.in);
+        carros09 meucarro = new carros09();
+
+        system out.println("cadastrar o veiculo");
+        system out.println("digite o modelo do carro");
+        meucarro.modelo = teclado.nextLine();
+
+        system out.println("digite a cor do carro")
+        meucarro.modelo = new carros09();
+
+        system out.println("digite a cor do carro");
+        meucarro.modelo = teclado.nextLine();
+
+        system out.println("digite a cor do carro");
+        meucarro.modelo = teclado.nextLine();
+
+        system out.println("digite a cor do carro");
+        meucarro.modelo = teclado.nextLine();
+    }
+}
