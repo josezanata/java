6ce62e212405802f4b3cf130d811f620a57517bd
josezanata <jose.krais@escola.pr.gov.br>
2026-06-29 09:07:11 -0300

    Add files via upload

---
+++ Arquivo/01_logica_basica/Aula03_CondicaoIf.java
@@ -0,0 +1,18 @@
+//package 01_logica_basica;
+
+public class Aula03_CondicaoIf {
+    public static void main(String[] args) {
+        int idade = 20;
+
+        // Condicional simples
+        if (idade >= 18) {
+            System.out.println("Você é maior de idade. Entrada permitida.");
+        }
+    }
+}
+
+2 halland
+36 halland
+45+2 halland
+74 halland
+83 halland
